67d30cf934161f7b29b0ecd41b7459b8ccedd381
Abhi Faldu <abhi.faldu@stud.th-rosenheim.de>
2026-05-10 17:38:50 +0200

    fix datasource UID and WritePrecision constant

--- detector/influx_writer.py
+++ detector/influx_writer.py
@@ -4,7 +4,7 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 
-INFLUX_URL = os.getenv("INFLUX_URL", "http://influxdb:8086")
+INFLUX_URL = os.getenv("INFLUX_URL", "http://localhost:8086")   # was influxdb
 INFLUX_TOKEN = os.getenv("INFLUX_TOKEN", "factory-token")
 INFLUX_ORG = os.getenv("INFLUX_ORG", "factory")
 INFLUX_BUCKET = os.getenv("INFLUX_BUCKET", "sensors")
@@ -41,7 +41,7 @@ class InfluxWriter:
         )
         self._write_api.write(
             bucket=INFLUX_BUCKET, org=INFLUX_ORG, record=point,
-            write_precision=WritePrecision.SECONDS,
+            write_precision=WritePrecision.S,
         )
 
     def close(self):
